a1d30114f173dbfee43046564dadaff471a64fc7
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-06-03 05:32:00 +0100

    Add files via upload

---
+++ src/java/Database/DAO.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.SessionScoped;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author DREAM INFO
+ * @param <T>
+ */
+
+public abstract class DAO<T> {
+
+    protected Database db;
+    
+   public abstract boolean create(T obj); 
+   public abstract ResultSet find(String id); 
+   public abstract ResultSet all(); 
+   public abstract boolean update(T obj , String id);
+   public abstract boolean delete (String id);
+
+
+//    @Override
+//    public int hashCode() {
+//        int hash = 0;
+//        hash += (id != null ? id.hashCode() : 0);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object object) {
+//        // TODO: Warning - this method won't work in the case the id fields are not set
+//        if (!(object instanceof DAO)) {
+//            return false;
+//        }
+//        DAO other = (DAO) object;
+//        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Database.DAO[ id=" + id + " ]";
+//    }
+//    
+}
